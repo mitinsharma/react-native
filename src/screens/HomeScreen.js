138f1bc3c49cdf134ea3706cbe9b26fc15ce7bfd
Mitin Sharma <er.mitinsharma@gmail.com>
2021-03-22 23:00:49 -0400

    Navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity, Touchable } from "react-native";
+import ComponentsScreen from "./ComponentsScreen";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi Mitin!</Text>;
-};
+const HomeScreen = (props) => {
+
+  return (
+  <View>
+    <Text style={styles.text}>Hi Mitin!</Text>
+    <Button 
+      onPress={() => props.navigation.navigate('Components')}
+      title="Go to components demo"
+    />
+    <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
+      <Text>Go to List Demo</Text>
+    </TouchableOpacity>
+  </View>
+  )};
 
 const styles = StyleSheet.create({
   text: {
